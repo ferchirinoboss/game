78b64c01d685bc60c8f53b3d7b48b1699aaa3eda
carlos <carlosquijadaxd@gmail.com>
2023-09-12 12:37:30 -0600

    Test

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import pygame 
+import pygame
+pygame.init()
+width, height = 500, 500
+Screen = pygame.display.set_mode((width, height))
+
+running = True
+
+while running is True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+            pygame.display.update()
